b28239bad27c845bc4a4b3adc943a603e004c640
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2020-10-07 20:12:26 +0900

    adds usd values

--- handler.ts
+++ handler.ts
@@ -170,107 +170,173 @@ export const statusCheck: APIGatewayProxyHandler = async (_event, _context) => {
 export const networkSnapshot: APIGatewayProxyHandler = async (_event, _context) => {
 
   const twitterClient = new TwitterClient();
-  // const baseUrl = await midgard();
+  let priceRes;
 
-    /**
-   * Fetch network data from midgard
-   */
-  
+  try {
+    priceRes = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=thorchain&vs_currencies=usd"); 
+  } catch (error) {
+    console.error('error fetching current price of RUNE');
+    console.error(error);
 
-   /** Temporarily until midgard update */
-  let baseUrl;
-  const ipsQuery = await fetch(`https://chaosnet-seed.thorchain.info`);
-  const ipsJson = await ipsQuery.json();
-
-  console.log('ipsJson is: ', ipsJson);
-
-  if (ipsJson && ipsJson.length > 0) {
-
-    const ip = ipsJson[Math.floor(Math.random() * ipsJson.length)];
-    baseUrl = `http://${ip}:8080`;
-
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'error fetching price of rune',
+      }, null, 2),
+    };
   }
 
-  /** end */
-  
-  if (baseUrl) {
+  if (priceRes) {
 
-    const req = await fetch(`${baseUrl}/v1/network`);
-    const json = await req.json();
-  
-    if (json && json.bondingROI && json.stakingROI && json.activeNodeCount && json.totalStaked && json.totalReserve) {
-  
-      const bondingROI = ((+json.bondingROI) * 100).toFixed(2);
-      const stakingROI = ((+json.stakingROI) * 100).toFixed(2);
-      const activeNodeCount = json.activeNodeCount;
-      const totalStaked = ((+json.totalStaked) / 10 ** 8).toFixed(0);
-      const totalReserve = ((+json.totalReserve) / 10 ** 8).toFixed(0);
-      let security;
-      let status: NetworkSecurityStatus;
-  
-      /**
-       * Network is up. Set security status
-       */
-      if (json.activeNodeCount > 0) {
-  
-        const activeBond = +json.bondMetrics.totalActiveBond;
-        security = activeBond / (activeBond + Number(json.totalStaked));
-  
-        if (0.9 <= security) {
-          status = NetworkSecurityStatus.INEFFICIENT;
-        } else if (0.75 < security && security < 0.9) {
-          status = NetworkSecurityStatus.OVERBONDED;
-        } else if (0.60 <= security && security <= 0.75) {
-          status = NetworkSecurityStatus.OPTIMAL;
-        } else if (0.50 <= security && security < 0.60) {
-          status = NetworkSecurityStatus.UNDERBONDED;
-        } else if (security < 0.50) {
-          status = NetworkSecurityStatus.INSECURE;
+    const priceJson = await priceRes.json();
+
+    if (priceJson && (priceJson.thorchain && priceJson.thorchain.usd)) {
+
+      const currentPrice = priceJson.thorchain.usd;
+
+        // const baseUrl = await midgard();
+
+          /**
+         * Fetch network data from midgard
+         */
+        
+
+        /** Temporarily until midgard update */
+        let baseUrl;
+        const ipsQuery = await fetch(`https://chaosnet-seed.thorchain.info`);
+        const ipsJson = await ipsQuery.json();
+
+        if (ipsJson && ipsJson.length > 0) {
+
+          const ip = ipsJson[Math.floor(Math.random() * ipsJson.length)];
+          baseUrl = `http://${ip}:8080`;
+
         }
-  
-      /**
-       * All Nodes are down
-       */
-      } else {
-  
-        status = NetworkSecurityStatus.DOWN;
-        security = 0;
-  
-      }
-  
-      const statusMessage = twitterClient.createStatusMessage(status);
-  
-      const message = `#THORChain #Chaosnet Network Snapshot:
+
+        /** end */
+        
+        if (baseUrl) {
+
+          const req = await fetch(`${baseUrl}/v1/network`);
+          const json = await req.json();
+        
+          if (json && json.bondingROI && json.stakingROI && json.activeNodeCount && json.totalStaked && json.totalReserve) {
+        
+            const bondingROI = ((+json.bondingROI) * 100).toFixed(2);
+            const stakingROI = ((+json.stakingROI) * 100).toFixed(2);
+            const activeNodeCount = json.activeNodeCount;
+            const totalStaked = ( ((+json.totalStaked) / 10 ** 8) * 2 );
+            const totalReserve = ((+json.totalReserve) / 10 ** 8);
+
+            const totalStakedUsd = (totalStaked * currentPrice).toFixed(0);
+            const totalReserveUsd = (totalReserve * currentPrice).toFixed(0);
+
+
+            const activeBonds = json.activeBonds.map( (bondStr) => formatAssetUnits( Number(bondStr), 8 ));
+            const standbyBonds = json.standbyBonds.map( (bondStr) => formatAssetUnits( Number(bondStr), 8 ) );
+            const totalBonded = calculateTotalBonded(activeBonds, standbyBonds);
+
+            const totalBondedUsd = (totalBonded * currentPrice).toFixed(0);
+            const totalCapitalUsd = ((totalStaked + totalBonded + totalReserve) * currentPrice).toFixed(0);
+
+
+
+
+            let security;
+            let status: NetworkSecurityStatus;
+        
+            /**
+             * Network is up. Set security status
+             */
+            if (json.activeNodeCount > 0) {
+        
+              const activeBond = +json.bondMetrics.totalActiveBond;
+              security = activeBond / (activeBond + Number(json.totalStaked));
+        
+              if (0.9 <= security) {
+                status = NetworkSecurityStatus.INEFFICIENT;
+              } else if (0.75 < security && security < 0.9) {
+                status = NetworkSecurityStatus.OVERBONDED;
+              } else if (0.60 <= security && security <= 0.75) {
+                status = NetworkSecurityStatus.OPTIMAL;
+              } else if (0.50 <= security && security < 0.60) {
+                status = NetworkSecurityStatus.UNDERBONDED;
+              } else if (security < 0.50) {
+                status = NetworkSecurityStatus.INSECURE;
+              }
+        
+            /**
+             * All Nodes are down
+             */
+            } else {
+        
+              status = NetworkSecurityStatus.DOWN;
+              security = 0;
+        
+            }
+        
+            const statusMessage = twitterClient.createStatusMessage(status);
+
+            const message = `#THORChain #Chaosnet Network Snapshot:
+
 Node Count: ${activeNodeCount}
-Staked: ${formatNumber(totalStaked)}
-Reserve: ${formatNumber(totalReserve)}
+Staked: $${formatNumber(totalStakedUsd)}
+Reserve: $${formatNumber(totalReserveUsd)}
+Bonded: $${formatNumber(totalBondedUsd)}
+Capital: $${formatNumber(totalCapitalUsd)}
 Bonding APY: ${bondingROI}%
 Staking APY: ${stakingROI}%
 Security Status: ${statusMessage}`;
-  
-      const tweet = await twitterClient.post(message);
-      if (tweet && tweet.data) {
-        console.log(`tweeted: ${tweet.data.text}`);
-      } else {
-        console.error('tweet unsuccessful');
-      }
-  
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: '👍 network snapshot cron run',
-        }, null, 2),
-      };
-    } else {
-      console.log('uh oh...');
+        
+            const tweet = await twitterClient.post(message);
+            if (tweet && tweet.data) {
+              console.log(`tweeted: ${tweet.data.text}`);
+            } else {
+              console.error('tweet unsuccessful');
+            }
+        
+            return {
+              statusCode: 200,
+              body: JSON.stringify({
+                message: '👍 network snapshot cron run',
+              }, null, 2),
+            };
+          } else {
+            console.log('uh oh...');
+          }
+
+        } else {
+          console.error('NO BASE URL!');
+        }
+
+
     }
 
-  } else {
-    console.error('NO BASE URL!');
+
   }
 
 }
 
 function formatNumber(num) {
   return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+function formatAssetUnits(value: number, unit: number): number {
+  return value / 10 ** unit;
+}
+
+function calculateTotalBonded(activeBonds: number[], standbyBonds: number[]): number {
+
+  let total = 0;
+
+  for (const bond of activeBonds) {
+    total += bond;
+  }
+
+  for (const bond of standbyBonds) {
+    total += bond;
+  }
+
+  return total;
+
 }
