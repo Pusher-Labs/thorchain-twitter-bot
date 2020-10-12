b30b798b79cbf704b86c9c8fe369a6173a565d0d
Kashif <static.kash@gmail.com>
2020-10-12 13:52:09 +0900

    add daily stats tweet bot

---
+++ handler-stats.ts
@@ -0,0 +1,67 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import * as fetch from "node-fetch";
+import { format as formatDate } from "date-fns";
+import * as commaNumber from "comma-number";
+import { TwitterClient } from "./clients/twitter-client";
+import { getRunePriceInUSD } from "./utils";
+
+const makeStatsMessage = async () => {
+  const response = await fetch(process.env.BEPSWAP_STATS_API);
+  const json = await response.json();
+
+  const priceInUSD = await getRunePriceInUSD();
+
+  //Convert to USD
+  [
+    "totalDepth",
+    "totalEarned",
+    "totalStaked",
+    "totalVolume",
+    "totalVolume24hr",
+  ].forEach((key) => {
+    const current = json[key];
+    if (current) {
+      json[key] = ((priceInUSD * current) / 10 ** 8).toFixed(1);
+    }
+  });
+
+  //Put commas
+  Object.entries(json).forEach(([k, v]) => (json[k] = commaNumber(v)));
+
+  const date = formatDate(new Date(), "PP");
+
+  const msg = `#THORChain ${date} Summary:
+Users:
+ Daily: ${json.dailyActiveUsers}
+ Monthly: ${json.monthlyActiveUsers}
+ Total: ${json.totalUsers}
+
+Txs:
+ Daily: ${json.dailyTx}
+ Monthly: ${json.monthlyTx}
+ Total: ${json.totalTx}
+
+Volume:
+ Last 24hr: $${json.totalVolume24hr}
+
+Pools: ${json.poolCount}
+Earned: $${json.totalEarned}
+Deposited: $${json.totalStaked}
+`;
+  return msg;
+};
+
+export const tweetStats: APIGatewayProxyHandler = async (_event, _context) => {
+  const twitterClient = new TwitterClient();
+  const message = await makeStatsMessage();
+  return twitterClient.post(message);
+};
+
+// For debugging purpose
+// export const debug: APIGatewayProxyHandler = async (_event, _context) => {
+//   const message = await makeStatsMessage();
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(message, null, 2),
+//   };
+// };
